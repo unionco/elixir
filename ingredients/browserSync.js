096251d2a0041b02736952b8a6412ec2b269aeb4
Brandon Poe <brandonpoe@me.com>
2015-03-23 08:35:35 -0400

    added browser-sync as ingredient

---
+++ ingredients/browserSync.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    _           = require('underscore'),
+    elixir      = require('union-elixir'),
+    config      = elixir.config;
+
+elixir.extend('browserSync', function (src, options) {
+  var defaultSrc = [
+    'app/**/*',
+    //'public/**/*',
+    'resources/views/**/*'
+  ];
+
+  src     = src || defaultSrc;
+  options = _.extend({
+    notify: false,
+    proxy: 'dev.example.com'
+  }, options);
+
+  gulp.task('browser-sync', function () {
+    if (browserSync.active === true) {
+      browserSync.reload();
+    } else if (gulp.tasks.watch.done === true) {
+      browserSync(options);
+    }
+  });
+
+  if (config.production === false) {
+    this.registerWatcher('browser-sync', src);
+  }
+
+  return this.queueTask('browser-sync');
+});
